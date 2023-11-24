2a8420db10928c4926b8f17f1a447d8900d47a50
Alejo Lang <alejolang@gmail.com>
2023-11-24 20:25:08 -0300

    player render

--- src/PlayScene.cpp
+++ src/PlayScene.cpp
@@ -1,13 +1,15 @@
 #include <SFML/Graphics.hpp>
 #include "PlayScene.h"
 
-PlayScene::PlayScene() {
-  
+PlayScene::PlayScene(){
+  if(!playerTexture.loadFromFile("./resources/textures/Starship.png")){ exit(1); }
+  playerShip.setTexture(playerTexture);
 }
 
 void PlayScene::update() {
-
+  playerShip.update();
 }
 
 void PlayScene::drawIn(sf::RenderWindow &window) {
+  playerShip.drawIn(window);
 }
