e782ec0a91466728ae6c96cf9adcb608e8735cb0
Alejo Lang <alejolang@gmail.com>
2023-11-24 20:25:33 -0300

    commit

--- src/Game.cpp
+++ src/Game.cpp
@@ -6,7 +6,7 @@ Game::Game()
   window.setFramerateLimit(60);
   window.setVerticalSyncEnabled(true);
   if(!gameFont.loadFromFile("./resources/fonts/Roboto-Regular.ttf")){exit(1);}
-  currentScene = new MenuScene(gameFont);
+  currentScene = new PlayScene();
   nextScene = nullptr;
 }
 
