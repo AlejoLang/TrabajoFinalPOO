d03e06aab2e8b6c862149cb35de02c0f735efd37
Alejo Lang <alejolang@gmail.com>
2023-11-24 20:25:57 -0300

    Add properties for the textures

--- include/PlayScene.h
+++ include/PlayScene.h
@@ -2,9 +2,14 @@
 #define PLAYSCENE_H
 #include <SFML/Graphics.hpp>
 #include "Scene.h"
+#include "Ship.h"
 
 class PlayScene : public Scene {
   private:
+    sf::View mainView;
+    sf::Texture playerTexture;
+    sf::Texture asteroidTexture;
+    Ship playerShip;
   public:
     PlayScene();
     void update() override;
