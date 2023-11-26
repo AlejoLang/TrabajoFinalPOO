6a8df9130c4130b72f5312728268586c7d91d792
Alejo Lang <alejolang@gmail.com>
2023-11-25 23:40:00 -0300

    Modify hud to include indicators

---
+++ include/Hud.h
@@ -0,0 +1,17 @@
+#ifndef HUD_H
+#define HUD_H
+#include <SFML/Graphics.hpp>
+#include "Ship.h"
+#include "Indicator.h"
+
+class Hud {
+  private:
+    Indicator fuelLOXIndicator;
+    Indicator fuelCH4Indicator;
+  public:
+    Hud(sf::Font &font);
+    void update(Ship &ship, sf::View &view);
+    void draw(sf::RenderWindow &window);
+};
+
+#endif
---
+++ src/Hud.cpp
@@ -0,0 +1,18 @@
+#include "Hud.h"
+
+Hud::Hud (sf::Font &font)
+    : fuelCH4Indicator(font, "CH4", 100)
+    , fuelLOXIndicator(font, "LOX", 350) {
+}
+
+void Hud::update(Ship &ship, sf::View &view) {
+  fuelCH4Indicator.update(ship.getCH4());
+  fuelLOXIndicator.update(ship.getLOX());
+  fuelCH4Indicator.setPos({view.getCenter().x + view.getSize().x / 2.0f - 100, view.getCenter().y + view.getSize().y / 2.0f - 100});
+  fuelLOXIndicator.setPos({view.getCenter().x + view.getSize().x / 2.0f - 100, view.getCenter().y + view.getSize().y / 2.0f - 150});
+}
+
+void Hud::draw(sf::RenderWindow &window){
+  fuelCH4Indicator.draw(window);
+  fuelLOXIndicator.draw(window);
+}
