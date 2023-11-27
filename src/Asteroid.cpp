a9c553c797e940b45b5037c9ce11bf9d4fcefa66
Alejo Lang <alejolang@gmail.com>
2023-11-27 01:39:52 -0300

    Fix asteroid spawing in the view

--- src/Asteroid.cpp
+++ src/Asteroid.cpp
@@ -7,9 +7,9 @@ Asteroid::Asteroid(sf::Texture &astText, sf::View &view) : Entity(astText) {
   int velocityMin = -4;
   int minX = view.getCenter().x - view.getSize().x / 2;
   int maxX = view.getCenter().x + view.getSize().x / 2;
-  entitySprite.setPosition(minX + rand()%(maxX - minX + 1), view.getCenter().y - view.getSize().y / 2 - astText.getSize().y);
   float scale = static_cast<int>(std::round(rand() % 4 + 1));
   entitySprite.setScale(scale, scale);
+  entitySprite.setPosition(minX + rand()%(maxX - minX + 1), view.getCenter().y - view.getSize().y / 2 - entitySprite.getGlobalBounds().height - 100);
   asteroidVelocity = {velocityMin + rand()%(velocityMax - velocityMin + 1), abs(velocityMin + rand()%(velocityMax - velocityMin + 1))};
 }
 
