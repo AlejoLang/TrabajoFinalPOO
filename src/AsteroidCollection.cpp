78e6994bbec667bfa0ff83e4756156d6dda768a8
Alejo Lang <alejolang@gmail.com>
2023-11-26 22:12:37 -0300

    Put the asteroids in a AsteroidCollection class

---
+++ src/AsteroidCollection.cpp
@@ -0,0 +1,55 @@
+#include "AsteroidCollection.h"
+
+AsteroidCollection::AsteroidCollection() : asteroidGenerationTime(0.5) {
+  for (size_t i = 0; i < 2; i++) {
+    sf::Texture aux;
+    asteroidTexures.push_back(aux);
+    if(!asteroidTexures[i].loadFromFile("./resources/textures/Asteroid"+ std::to_string(i + 1)+".png")) { exit(1); }
+  }
+}
+
+void AsteroidCollection::update(sf::View &mainView, Ship *playerShip) {
+  handleAsteroidGeneration(mainView);
+  for (Asteroid *a: asteroidsVector) {
+    a->update();
+  }
+  handleAsteroidCollision(playerShip);
+  handleAsteroidRemove(mainView);
+}
+
+void AsteroidCollection::handleAsteroidGeneration(sf::View &mainView) {
+  if(asteroidGenerationClock.getElapsedTime().asSeconds() > asteroidGenerationTime) {
+    Asteroid *newAsteroid = new Asteroid(asteroidTexures[static_cast<int>(std::round(rand() % 2))], mainView);
+    asteroidsVector.push_back(newAsteroid);
+    asteroidGenerationClock.restart();
+  }
+}
+
+void AsteroidCollection::handleAsteroidCollision(Ship *playerShip) {
+  for (Asteroid *a: asteroidsVector) {
+    if(Collision::pixelPerfectTest(a->getSprite(), playerShip->getSprite())){
+      exit(1);
+    }
+  }
+}
+
+void AsteroidCollection::handleAsteroidRemove(sf::View &mainView) {
+  for (int i = 0; i < asteroidsVector.size(); ++i) {
+    if(asteroidsVector[i]->getCenter().y - asteroidsVector[i]->getSprite().getGlobalBounds().height > mainView.getCenter().y + mainView.getSize().y / 2.0f){
+      delete asteroidsVector[i];
+      asteroidsVector.erase(asteroidsVector.begin() + i);
+    }
+  }
+}
+
+void AsteroidCollection::drawIn(sf::RenderWindow &window) {
+  for (Asteroid *a: asteroidsVector) {
+    a->drawIn(window);
+  }
+}
+
+AsteroidCollection::~AsteroidCollection(){
+  for (Asteroid *a: asteroidsVector) {
+    delete a;
+  }
+}
--- src/PlayScene.cpp
+++ src/PlayScene.cpp
@@ -1,20 +1,15 @@
 #include <SFML/Graphics.hpp>
 #include "PlayScene.h"
 #include <random>
+#include "Collision.h"
 
 PlayScene::PlayScene(sf::RenderWindow &window, sf::Font &gameFont) 
           : mainMap(window)
           , mainView(window.getDefaultView())
           , mainHud(gameFont)
-          , asteroidGenerationTime(0.3)
 {
   if(!playerTexture.loadFromFile("./resources/textures/Starship.png")){ exit(1); }
   if(!backgroundTexture.loadFromFile("./resources/textures/Background.png")) { exit(1); }
-  for (size_t i = 0; i < 2; i++) {
-    sf::Texture aux;
-    asteroidTexures.push_back(aux);
-    if(!asteroidTexures[i].loadFromFile("./resources/textures/Asteroid"+ std::to_string(i + 1)+".png")) { exit(1); }
-  }
   playerShip = new Ship(playerTexture, window);
   background.setTexture(backgroundTexture);
   background.setOrigin(backgroundTexture.getSize().x / 2.0f, backgroundTexture.getSize().y / 2.0f);
@@ -22,8 +17,8 @@ PlayScene::PlayScene(sf::RenderWindow &window, sf::Font &gameFont)
 
 void PlayScene::update() {
   playerShip->update(mainMap);
-  handleAsteroidsGeneration();
   mainView.setCenter(playerShip->getCenter().x, playerShip->getCenter().y - 350.f);
+  mainAsteroidCollection.update(mainView, playerShip);
   mainHud.update(playerShip, mainView);
   background.setPosition(mainView.getCenter());
 }
@@ -32,39 +27,13 @@ void PlayScene::drawIn(sf::RenderWindow &window) {
   window.setView(mainView);
   window.draw(background);
   playerShip->drawIn(window);
-  window.draw(playerShip->getRect());
-  handleAsteroidsDraw(window);
+  mainAsteroidCollection.drawIn(window);
   mainMap.drawIn(window);
   mainHud.draw(window);
 }
 
-void PlayScene::handleAsteroidsGeneration() {
-  /*if(asteroidGenerationClock.getElapsedTime().asSeconds() > asteroidGenerationTime) {
-    Asteroid *newAsteroid = new Asteroid(asteroidTexures[static_cast<int>(std::round(rand() % 2))], mainView);
-    asteroidsVector.push_back(newAsteroid);
-    asteroidGenerationClock.restart();
-  }
-  for (Asteroid *a: asteroidsVector)
-  {
-    sf::RectangleShape shipSpt = playerShip->getRect();
-    a->update();
-    if(a->checkCollisionWithObject(shipSpt)){
-      exit(1);
-    }
-  }*/
-  
-}
-
-void PlayScene::handleAsteroidsDraw(sf::RenderWindow &window) {
-  for(Asteroid *a: asteroidsVector) {
-    a->drawIn(window);
-    window.draw(a->getCollisionBox());
-  }
-}
 
 PlayScene::~PlayScene() {
   delete playerShip;
-  for (Asteroid *a: asteroidsVector){
-    delete a;
-  }
+  
 }
