f516afa5a29a6249155d33d5298ef8bf10aac569
Alejo Lang <alejolang@gmail.com>
2024-01-02 18:00:11 -0300

    Change some parameters

--- src/Asteroid.cpp
+++ src/Asteroid.cpp
@@ -3,11 +3,11 @@
 #include <math.h>
 
 Asteroid::Asteroid(sf::Texture &astText, sf::View &view) : Entity(astText) {
-  int velocityMax = 4;
-  int velocityMin = -4;
+  int velocityMax = 7;
+  int velocityMin = -7;
   int minX = view.getCenter().x - view.getSize().x / 2;
   int maxX = view.getCenter().x + view.getSize().x / 2;
-  float scale = static_cast<int>(std::round(rand() % 4 + 1));
+  float scale = static_cast<int>(std::round(rand() % 3 + 2));
   entitySprite.setScale(scale, scale);
   entitySprite.setPosition(minX + rand()%(maxX - minX + 1), view.getCenter().y - view.getSize().y / 2 - entitySprite.getGlobalBounds().height - 100);
   asteroidVelocity = {velocityMin + rand()%(velocityMax - velocityMin + 1), abs(velocityMin + rand()%(velocityMax - velocityMin + 1))};
