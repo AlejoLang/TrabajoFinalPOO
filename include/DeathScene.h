db0590fd671a6ee40fecd011fd2687aeff68b6a0
unknown <alejolang123@gmail.com>
2024-01-14 14:33:27 -0300

    Add wrapper function and comments

--- include/DeathScene.h
+++ include/DeathScene.h
@@ -12,6 +12,7 @@ class DeathScene : public Scene {
   public:
     DeathScene(Game &game, sf::RenderWindow &window, int points, sf::Font* gameFont);
     void update(Game &game, sf::RenderWindow &window) override;
+    void checkEvents(Game& game, sf::RenderWindow& window);
     void processEvent(sf::Event &ev) override;
     void updateHighscores(Game &game, int points);
     void drawIn(sf::RenderWindow &window) override;
--- src/DeathScene.cpp
+++ src/DeathScene.cpp
@@ -12,29 +12,42 @@ DeathScene::DeathScene(Game &game, sf::RenderWindow &window, int points, sf::Fon
           , backToMenu("Menu", gameFont)
           , restartGame("Restart", gameFont)
 {
+  /* Set title text */
   mainText.setString("YOU DIED");
   mainText.setFont(*sceneFont);
   mainText.setFillColor(sf::Color::Red);
   mainText.setCharacterSize(72);
   mainText.setOrigin(getCenter(mainText));
   mainText.setPosition(window.getSize().x / 2.0f, window.getSize().y / 2.0f  - 200);
+
+  /* Set subtitle text */
   pointsText.setString("Total points: " + std::to_string(points));
   pointsText.setFont(*sceneFont);
   pointsText.setCharacterSize(42);
   pointsText.setOrigin(getCenter(pointsText));
   pointsText.setPosition(getPlaceBelow(mainText, pointsText, 50));
+
+  /* Set buttons position */
   backToMenu.setPos(getPlaceBelow(pointsText, backToMenu.getBox(), 100));
   restartGame.setPos(getPlaceBelow(backToMenu.getBox(), restartGame.getBox(), 20));
+
   updateHighscores(game, points);
 }
 
 void DeathScene::update(Game &game, sf::RenderWindow &window) {
+  /* Set buttons color on hover*/
   backToMenu.update(window);
   restartGame.update(window);
-  if(backToMenu.isClicked(sceneEvent, window)){
+
+  /* Check for keyboard or mouse button press events */
+  checkEvents(game, window);
+}
+
+void DeathScene::checkEvents(Game& game, sf::RenderWindow& window) {
+  if (backToMenu.isClicked(sceneEvent, window)) {
     game.changeScene(new MenuScene(game, window, sceneFont));
   }
-  if(restartGame.isClicked(sceneEvent, window)){
+  if (restartGame.isClicked(sceneEvent, window)) {
     game.changeScene(new PlayScene(game, window, sceneFont));
   }
   if (sceneEvent.key.scancode == sf::Keyboard::Scan::Escape) {
@@ -47,9 +60,11 @@ void DeathScene::processEvent(sf::Event &ev){
 }
 
 void DeathScene::updateHighscores(Game &game, int points) {
+  /* Put default username if the username is blank */
   if(game.getUsername() == "") {
     game.setUsername("Player");
   }
+  
   HighscoreStruct newHs(game.getUsername(), points);
   game.getHighscores().addScore(newHs);
 }
