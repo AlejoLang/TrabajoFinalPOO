28a7ea52ba2a422294dc6eb8fd1261a078c8426b
unknown <alejolang123@gmail.com>
2024-01-14 14:40:56 -0300

    Add comments

--- src/PlayScene.cpp
+++ src/PlayScene.cpp
@@ -15,33 +15,54 @@ PlayScene::PlayScene(Game &game, sf::RenderWindow &window, sf::Font* gameFont)
           , mainMap(window)
           , mainHud(gameFont)
           , mainRefillerCollection(game)
-          , points(0)
 { 
+  /* Load textures */
   if(!playerTexture.loadFromFile("./resources/textures/Starship.png")){ exit(1); }
   if(!backgroundTexture.loadFromFile("./resources/textures/background.png")){ exit(1); }
+
+  /* Create player */
   playerShip = new Ship(game, playerTexture, mainMap);
+
+  /* Set up background */
   background.setTexture(backgroundTexture);
   background.setTextureRect(sf::IntRect(0, backgroundTexture.getSize().y - 1080, backgroundTexture.getSize().x, backgroundTexture.getSize().y));
   background.setOrigin(window.getSize().x / 2.0f, window.getSize().y / 2.0f + 50);
+
+  /* Initialize variables */
   deadFrameCount = 0;
+  points = 0;
 }
 
 void PlayScene::update(Game &game, sf::RenderWindow &window) {
+  /* Update player */
   playerShip->update(window, mainMap);
+
+  /* Center view on player */
   mainView.setCenter(playerShip->getCenter().x, playerShip->getCenter().y - 350.f);
+
+  /* Update obstavles and refillers collection*/
   mainObstaclesCollection.update(mainView, playerShip, game.getGameDifficulty());
   mainRefillerCollection.update(mainView, playerShip, game.getGameDifficulty());
+
+  /* Update hud */
   mainHud.update(playerShip, mainView);
+
   updateBackground(playerShip->getAltitudeKm());
   updateGameDifficulty(game, playerShip->getAltitudeKm());
   updatePoints();
+
+  /* Handle player dead */
   if(!playerShip->getStatus()){
+    /* Wait until the explosion animation and the explosion sound stops playing and then change to the death scene */
     if(!playerShip->isExplosionSoundPlaying() && deadFrameCount > 12) {
       game.changeScene(new DeathScene(game, window, points, sceneFont));
     } else {
       deadFrameCount++;
     }
-  } else if (sceneEvent.key.code == sf::Keyboard::Escape) {
+  }
+
+  /* Check esc key press to go back to the menu */
+  if (sceneEvent.key.code == sf::Keyboard::Escape) {
     game.changeScene(new MenuScene(game, window, sceneFont));
   }
 }
@@ -51,6 +72,7 @@ void PlayScene::processEvent(sf::Event &ev) {
 }
 
 void PlayScene::updateBackground(float altitude) {
+  /* Update the texture rect of the background based on altitude */
   if(altitude < 10) {
     if(background.getTextureRect().top < int(background.getTexture()->getSize().y) - 1080) {
       background.setTextureRect({0, background.getTextureRect().top + 3, background.getTextureRect().width, background.getTextureRect().height + 3});
@@ -77,21 +99,12 @@ void PlayScene::updateBackground(float altitude) {
   background.setPosition(mainView.getCenter());
 }
 
-void PlayScene::drawIn(sf::RenderWindow &window) {
-  window.setView(mainView);
-  window.draw(background);
-  playerShip->drawIn(window);
-  mainObstaclesCollection.drawIn(window);
-  mainRefillerCollection.drawIn(window);
-  mainMap.drawIn(window);
-  mainHud.draw(window);
-}
-
 void PlayScene::updatePoints() {
   points = int((playerShip->getAltitudeKm() * 100) + ((playerShip->getCH4() + playerShip->getLOX()) * 0.5));
 }
 
 void PlayScene::updateGameDifficulty(Game &game, float altitude) {
+  /* Set a number for the game difficulty based on altitude */
   if(altitude < 10) {
     game.setGameDifficulty(1);
   } else if( altitude < 20) {
@@ -103,7 +116,18 @@ void PlayScene::updateGameDifficulty(Game &game, float altitude) {
   }
 }
 
+void PlayScene::drawIn(sf::RenderWindow& window) {
+  window.setView(mainView);
+  window.draw(background);
+  playerShip->drawIn(window);
+  mainObstaclesCollection.drawIn(window);
+  mainRefillerCollection.drawIn(window);
+  mainMap.drawIn(window);
+  mainHud.draw(window);
+}
+
 PlayScene::~PlayScene() {
+  /* Remove the player */
   if (playerShip != nullptr) {
     delete playerShip;
     playerShip = nullptr;
